c35b9d4274d43fab97abd2bc630847195f9457ad
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-10-02 18:57:00 +0200

    add dialog system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PlayerComponent } from './player/player.component';
+import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [AppComponent, StartScreenComponent, GameComponent, PlayerComponent],
+  declarations: [
+    AppComponent,
+    StartScreenComponent,
+    GameComponent,
+    PlayerComponent,
+    DialogAddPlayerComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
+    MatDialogModule,
     BrowserAnimationsModule,
   ],
   providers: [],
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent {
-
+  @Input() name: any;
 }
